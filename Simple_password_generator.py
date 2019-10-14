03fd8791a186f6a00b3604a5c43c2feef925a384
Igorxyz <igor@test_xubuntu.xyz>
2019-10-14 08:32:17 +0200

    Added error checks, removed option

--- Simple_password_generator.py
+++ Simple_password_generator.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python3
 
-import time
 import sys
 import random
+import getpass
+import time
+start = time.time()
 
 password_options = ["1234567890", "abcdefghijklmopqrstxyz", "!@#$%", "abcdefghijklmopqrstxyz".upper()]
 
 print("CLI PASSWORD GENERATOR")
 print("*" * 80)
-user_choice = input('Would you like to generate password? y/n ')
 print()
-if (user_choice in ("yes", "y")):
+
+try:
     password_length = int(input("Choose password length: "))
     characters_list = []
     for i in range(0, password_length):
         characters_list.append(random.choice("".join(password_options)))
     print("Generated password: {}".format("".join(characters_list)))
-else:
-    print("Exiting program...")
+except KeyboardInterrupt:
+    print()
+    print("Program interrupted by the user: {}".format(getpass.getuser()))
+    print("Terminating program...")
     sys.exit()
+except ValueError:
+    print()
+    print("Password length has to be a number")
+    sys.exit()
+
+end = time.time()
+print("Execution time: {} seconds".format(round(float(end - start),2)))
+
